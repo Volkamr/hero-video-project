5bc9471a07a0099dfa19de463928617c7384df57
Juan Volkmar <juan.volkmar@upb.edu.co>
2024-06-16 17:34:50 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://volkmar.github.io/hero-video-project",
 })
